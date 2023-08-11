3f8adee6199441b7375aa27dbeb603915dd9533f
Hudson390 <hudsoncdiniz@gmail.com>
2023-08-11 20:25:36 -0300

    Params

--- Index.js
+++ Index.js
@@ -2,12 +2,19 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("Seja bem vindp ao meu app");
+    res.send("Seja bem vindo ao meu app");
 });
 
 app.get("/sobre", function(req, res){
     res.send("Pagina sobre app");
 });
 
+app.get("/blog", function(req, res){
+    res.send("Bem vindo ao meu blog");
+});
 
-app.listen(8081)
+app.get("/ola/:nome/:cargo", function(req, res){
+    res.send("<h1>Ola " + req.params.nome+"</h1>"+"\n<h2>Seu cargo e: "+req.params.cargo+"</h2>");
+})
+
+app.listen(8081); 
